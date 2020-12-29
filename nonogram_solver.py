f98e1aed7ce083305a004dc409130759fd25ce26
David Chen <davidc3287@gmail.com>
2020-12-29 14:45:40 -0500

    initial class setup

---
+++ nonogram_solver.py
@@ -0,0 +1,9 @@
+class NonogramSolver:
+    def solve_line(line, rules):
+        """ Given a line (row/col) and rules, solve as much as possible """
+        return 5
+
+
+if __name__ == "__main__":
+    nonogram_solver = NonogramSolver()
+    nonogram_solver.solve_line(['', '', '', '', ''], [5])
